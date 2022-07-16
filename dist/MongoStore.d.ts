12776c9e04bea9288dddc442b90ddcec60b973df
Jakub Tomáš <jakubtomas108@gmail.com>
2022-07-16 21:18:49 +0200

    Add store from common module

--- dist/MongoStore.d.ts
+++ dist/MongoStore.d.ts
@@ -1,3 +1,3 @@
 import { Store } from "@apikee/authenticator-common";
-
-export class MongoStore extends Store {}
+export declare class MongoStore extends Store {
+}
---
+++ dist/MongoStore.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MongoStore = void 0;
+const authenticator_common_1 = require("@apikee/authenticator-common");
+class MongoStore extends authenticator_common_1.Store {
+}
+exports.MongoStore = MongoStore;
